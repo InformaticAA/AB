c1032c7da7640c40cb75bd169d1b2ac875dbfc81
schreilem <566400@unizar.es>
2015-05-25 17:17:04 +0200

    Fuerza Bruta iniciado

--- AB/src/practica2/FuerzaBruta.java
+++ AB/src/practica2/FuerzaBruta.java
@@ -1,5 +1,11 @@
 package practica2;
 
+import java.util.ArrayList;
+
 public class FuerzaBruta {
 
+	public static int fuerzaBruta(int[][] matriz, ArrayList<Integer> visitados, int inicial){
+		
+		int numVert = matriz.length;
+	}
 }
--- AB/src/practica2/TSP.java
+++ AB/src/practica2/TSP.java
@@ -1,5 +1,15 @@
 package practica2;
 
+import java.util.ArrayList;
+
 public class TSP {
 
+	public static void main(String[] args){
+		int numVert = 0;
+		int[][] matriz = new int[numVert][numVert];
+		ArrayList<Integer> visitados = new ArrayList<Integer>();
+		
+		fuerzaBruta(matriz,visitados,0);
+		
+	}
 }
