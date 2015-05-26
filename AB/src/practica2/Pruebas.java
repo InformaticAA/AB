0e5faf256eeb95e5b31f8f3966a511f25408aacb
Alejandro <Alejandro@Shadow-Moses>
2015-05-26 22:21:41 +0200

    Pruebas comentada y bien formateada. Faltara implementar el algoritmo de programacion dinamica

--- AB/src/practica2/Pruebas.java
+++ AB/src/practica2/Pruebas.java
@@ -25,6 +25,7 @@ public class Pruebas {
 			}
 			int[][] matriz = generarMatrizRandom(dimension,debug);
 			if(debug){
+				System.out.println("Ejecutando por fuerza bruta...");
 				System.out.println();
 				System.out.println("Posibles caminos   -  Coste");
 				System.out.println("===========================");
@@ -38,14 +39,41 @@ public class Pruebas {
 				System.out.println();
 				System.out.println("El coste minimo obtenido es: " + coste);
 				System.out.println();
-				System.out.printf("Tiempo empleado: %d milisegundos%n",tiempo);
+				System.out.printf("Tiempo empleado: %f milisegundos%n",tiempo);
+			}
+			
+			if(debug){
+				System.out.println("Ejecutando por programacion dinamica...");
+				System.out.println();
+				System.out.println("Posibles caminos   -  Coste");
+				System.out.println("===========================");
+			}
+			visitados = new ArrayList<Integer>();
+			inicio = System.currentTimeMillis();
+			coste = FuerzaBruta.fuerzaBruta(matriz,visitados,0,0,debug);				//Habra que cambiar a programacion dinamica
+			tiempo = System.currentTimeMillis() - inicio;
+			tiempoPD = tiempoPD + tiempo;
+			if(debug){
+				System.out.println();
+				System.out.println("El coste minimo obtenido es: " + coste);
+				System.out.println();
+				System.out.printf("Tiempo empleado: %f milisegundos%n",tiempo);
 			}
 		}
 		System.out.println();
 		System.out.printf("Tiempo total fuerza bruta: %d milisegundos%n",tiempoFB);
-		System.out.printf("Media fuerza bruta: %d milisegundos%n",tiempoFB/numPruebas);
+		System.out.printf("Media fuerza bruta: %.2f milisegundos%n",((double)tiempoFB/numPruebas));
+		System.out.println();
+		System.out.printf("Tiempo total fuerza bruta: %d milisegundos%n",tiempoPD);
+		System.out.printf("Media fuerza bruta: %.2f milisegundos%n",((double)tiempoPD/numPruebas));
+		
+		
 	}
 	
+	/**
+	 * Pide por teclado un numero (con el mensaje pasado como parametro 
+	 * y lo devuelve
+	 */
 	private static int preguntar(String mensaje){
 		System.out.printf(mensaje);
 		Scanner teclado = new Scanner(System.in);
@@ -53,13 +81,23 @@ public class Pruebas {
 		return numIntervalos;
 	}
 	
+	/**
+	 * Pide al usuario si quiere el modo debug. En caso de ser la respuesta "Y",
+	 * devuelve true. En caso contrario, false
+	 */
 	private static boolean preguntarDebug(){
-		System.out.printf("¿Desea activar el modo debug? (Y/N) ");
+		System.out.printf("¿Desea activar el modo debug? (Y/N): ");
 		Scanner teclado = new Scanner(System.in);
 		String debug = teclado.next();
 		return debug.equalsIgnoreCase("Y");
 	}
 	
+	
+	/**
+	 * Genera una matriz cuadrada con la dimension pasada por parametro, diagonal 0
+	 * y resto de elementos entre 1 y 15. En caso de estar el debug activado, imprime
+	 * por pantalla el contenido de la matriz
+	 */
 	private static int[][] generarMatrizRandom(int dimension,boolean debug){
 		int[][] matriz = new int[dimension][dimension];
 		for (int i = 0; i < dimension; i++) {
@@ -81,6 +119,9 @@ public class Pruebas {
 		return matriz;
 	}
 	
+	/**
+	 * Genera un entero entre 1 y 15
+	 */
 	private static int generarDatoRandom(){
 		Random rand = new Random(); 
 		int pit = rand.nextInt(15) + 1;
